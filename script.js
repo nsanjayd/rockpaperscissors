d686ab1b18d42c79bd263d3c698ce78f8054c9c6
nsanjayd <nicholasdesai@gmail.com>
2022-05-06 17:55:42 -0400

    Finished

--- script.js
+++ script.js
@@ -9,36 +9,49 @@ function playRound() {
     playerPrompt = prompt("Enter rock, paper, or scissors.");
     playerSelection = (playerPrompt[0]).toUpperCase() + (playerPrompt.slice(1)).toLowerCase();
     i = computerPlay();
-    let tally = [];
     if (playerSelection === i) {
         return "It's a tie."
-        tally.push("Tie");
     } else if (playerSelection === "Rock" && i === "Scissors") {
         return "You win. Rock beats scissors."
-        tally.push("Win");
     } else if (playerSelection === "Rock" && i === "Paper") {
         return "You lose. Paper beats rock."
-        tally.push("Lose");
     } else if (playerSelection === "Paper" && i === "Scissors") {
         return "You lose. Scissors beats paper."
-        tally.push("Lose");
     } else if (playerSelection === "Paper" && i === "Rock") {
         return "You win. Paper beats rock."
-        tally.push("Win");
     } else if (playerSelection === "Scissors" && i === "Paper") {
         return "You win. Scissors beats paper."
-        tally.push("Win");
     } else if (playerSelection === "Scissors" && i === "Rock") {
         return "You lose. Rock beats scissors."
-        tally.push("Lose");
     }
     console.log(tally);
 }
 
-
 function game() {
-    for (let a = 0; 0 < 5; a++) {
-        playRound();
+    loss = [];
+    win = [];
+    tie = [];
+    for (let a = 0; a < 5; a++) {
+        c = playRound();
+        console.log(c);
+        if (c === "It's a tie.") {
+            tie.push(c);
+        } else if (c === "You win. Rock beats scissors." || c === "You win. Paper beats rock." || c === "You win. Scissors beats paper.") {
+            win.push(c);
+        } else if (c === "You lose. Paper beats rock." || c === "You lose. Scissors beats paper." || c === "You lose. Rock beats scissors.") {
+            loss.push(c);
+        }
     }
+   
+    if (win.length > loss.length) {
+        result = "you won."
+    } else if (win.length = loss.length) {
+        result = "you tied."
+    } else {
+        result = "you lost."
+    }
+
+
+    console.log(`In five rounds, ${result} The tally is: \n Wins: ${win.length} \n Losses: ${loss.length} \n Ties: ${tie.length}`)
 
 }
